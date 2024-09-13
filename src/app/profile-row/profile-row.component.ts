35c39323572c77a3f53150082979e4113ee0b112
Deniz Saganak <saganak.deniz@gmail.com>
2024-09-13 17:00:58 +0200

    add if conditions for adding friends

--- src/app/profile-row/profile-row.component.ts
+++ src/app/profile-row/profile-row.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FriendService } from '../friend.service';
 
 @Component({
   selector: 'app-profile-row',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile-row.component.html',
   styleUrl: './profile-row.component.scss'
 })
@@ -12,6 +13,7 @@ export class ProfileRowComponent {
   @Input() name = 'Frederick';
   @Input() img = 'hamster1.jpg';
   @Input() description = '3 Jahre alt';
+  @Input() canFollow = true;
 
   constructor(public fs: FriendService) {}
 }
